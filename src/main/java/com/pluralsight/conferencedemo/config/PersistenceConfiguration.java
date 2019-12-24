e9bcdd09f4cdf480d0db4ac17cb448c038df5f20
Siddhartha Jain <sjsidjain213@gmail.com>
2019-12-24 11:50:54 +0530

    Update PersistenceConfiguration.java

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -10,10 +10,10 @@ import javax.sql.DataSource;
 public class PersistenceConfiguration {
     @Bean
     public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        System.out.println("My custom datasource bean has been initialized and set");
-        return builder.build();
+//         DataSourceBuilder builder = DataSourceBuilder.create();
+//         builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//         System.out.println("My custom datasource bean has been initialized and set");
+//         return builder.build();
     }
 
 }
